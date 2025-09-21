baa14995ce82ee82f91261425d0d160a3dfa5bcf
Wagner Barbosa <wsoubar@gmail.com>
2025-09-21 14:10:25 -0300

    inicial

---
+++ src/test/java/br/com/wsoubar/earth/EarthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wsoubar.earth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
