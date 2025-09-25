35dadba0ebb2c4f4311900430fa9498bb7223455
Wagner Barbosa <wsoubar@gmail.com>
2025-09-25 18:12:57 -0300

    mais ajustes

--- src/main/java/br/com/wsoubar/earth/EarthApplication.java
+++ src/main/java/br/com/wsoubar/earth/EarthApplication.java
@@ -6,12 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EarthApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EarthApplication.class);
-    }
+public class EarthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EarthApplication.class, args);
